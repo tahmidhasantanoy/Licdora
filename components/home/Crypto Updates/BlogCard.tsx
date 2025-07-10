83af272880d7968b5aabb277971cc46ebe9d0409
tahmidhasantanoy <tahmid.hasan.tanoy@gmail.com>
2025-07-10 12:38:52 +0600

    Style: crypto updates section style changed

--- components/home/Crypto Updates/BlogCard.tsx
+++ components/home/Crypto Updates/BlogCard.tsx
@@ -2,7 +2,7 @@
 import { Badge } from '@/components/ui/badge';
 import { Card } from '@/components/ui/card';
 import Image from 'next/image';
-import React from 'react';
+import cardImage from "../../../public/images/picture.png"
 
 const BlogCard = ({
           image,
@@ -46,7 +46,9 @@ const BlogCard = ({
         
         
           return (
-            <div className='group bg-opacity-80 .hover:bg-gradient-to-br from-red-500 via-orange-500 to-red-600 rounded-3xl p-0 duration-500 .transition-transform transition-all'
+            /* w-[392px] h-[517px] pt-4 pr-4 pb-8 pl-4 gap-[24px] rounded-[40px] border border-solid */
+            /* Hover work with padding */
+            <div className='group p-0 max-w-sm h-[517px] bg-opacity-80 .hover:bg-gradient-to-br from-red-500 via-orange-500 to-red-600 rounded-[40px]  border border-solid duration-500 .transition-transform transition-all'
             style={{
               background: `linear-gradient(to bottom, 
                 #FF3C3C 0px, 
@@ -56,25 +58,28 @@ const BlogCard = ({
                 #FF3C3C 120px, 
                 #FF3C3C 100%)`,
               // padding: '1px', // Thick border here
-              borderRadius: '10px',
+              borderRadius: '40px',
               }}>
                 <Card className="bg-black border-gray-800 rounded-3xl p-6 group-hover:m-[1px]  transition-colors">
               <div className="relative w-full h-48 md:h-56 mb-4 rounded-2xl overflow-hidden">
-                <Image src={image || "/placeholder.svg"} alt={title} fill className="object-cover" />
+                <Image src={cardImage || "/placeholder.svg"}
+                  width={360}
+                  height={300}
+                  alt={title} 
+                  className="object-cover rounded-2xl" />
                 {/* GET http://localhost:3000/_next/image?url=%2Fcrypto-security.png&w=1920&q=75 400 (Bad Request) */}
               </div>
         
               <div className="text-right mb-4">
-                <span className="text-gray-400 text-sm">{date}</span>
+                <span className="font-normal text-sm leading-7 text-right text-[#EEEEEE]">{date}</span>
               </div>
         
               <div className="mb-4">
-                <Badge variant="outline" className={`${categoryColor} bg-transparent hover:bg-gray-800 rounded-3xl px-3 py-[2.5px]`}>
+                <Badge variant="outline" className={`${categoryColor} bg-transparent hover:bg-gray-800 rounded-3xl px-3 py-1.5 border-[.5px] border-[#E59174]`}>
                   {category}
                 </Badge>
               </div>
-        
-              <h3 className="text-white text-xl font-semibold leading-tight">{title}</h3>
+              <h3 className="font-medium text-white text-xl leading-8">{title}</h3>
             </Card>
         </div>
           )
--- components/home/trending pairs/TrendingSection.tsx
+++ components/home/trending pairs/TrendingSection.tsx
@@ -98,7 +98,7 @@ const TrendingSection = () => {
         <SectionHeader sectionHeaderData={sectionHeaderData}/>
 
         {/* Exchange Pairs Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 .px-6 gap-x-6 mb-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-4 mb-16">
           {trendingPairsData.exchangePairs.map((pair) => (
             <ExchangePairCard
               key={pair.id}
